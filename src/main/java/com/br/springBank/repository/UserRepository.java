290eef320658f2ca58cd431949c8fada085d9548
LucaasHenrique <lucashsouza101@gmail.com>
2025-03-26 11:02:20 -0300

    feat: adicionando método existsByEmail

--- src/main/java/com/br/springBank/repository/UserRepository.java
+++ src/main/java/com/br/springBank/repository/UserRepository.java
@@ -2,8 +2,10 @@ package com.br.springBank.repository;
 
 import com.br.springBank.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Boolean existsByEmail(String email);
 }
