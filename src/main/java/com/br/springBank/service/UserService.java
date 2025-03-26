7a5d7f93cf06915fcd93697ba00069090828f3a4
LucaasHenrique <lucashsouza101@gmail.com>
2025-03-26 11:03:27 -0300

    feat: adicionando verificação de email e senha

--- src/main/java/com/br/springBank/service/UserService.java
+++ src/main/java/com/br/springBank/service/UserService.java
@@ -5,7 +5,6 @@ import com.br.springBank.mapper.UserMapper;
 import com.br.springBank.model.types.UserRole;
 import com.br.springBank.repository.UserRepository;
 import org.springframework.stereotype.Service;
-
 import java.util.UUID;
 
 @Service
@@ -18,6 +17,14 @@ public class UserService {
     }
 
     public UUID createUser(UserDto userDto) {
+        if (userDto.password() == null) {
+            throw new IllegalArgumentException("password cannot be null");
+        }
+
+        if (userRepository.existsByEmail(userDto.email())) {
+            throw new IllegalArgumentException("Email already in use");
+        }
+
         var user = UserMapper.INSTANCE.userDtoToUser(userDto);
         user.setRole(UserRole.USER);
         userRepository.save(user);
