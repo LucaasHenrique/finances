fd786843bac21e820c7470ca60dc7acf0c57f3a5
LucaasHenrique <lucashsouza101@gmail.com>
2025-03-26 10:59:04 -0300

    feat: tornando email único no banco de dados

--- src/main/java/com/br/springBank/model/User.java
+++ src/main/java/com/br/springBank/model/User.java
@@ -20,6 +20,7 @@ public class User implements Serializable {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
+    @Column(unique = true)
     private String email;
     private String password;
     @CreationTimestamp
